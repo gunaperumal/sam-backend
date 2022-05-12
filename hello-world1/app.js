784cee8f739bce2e6d2253c5dcccbf8fb11078f5
gunaperumal <guna.p@aikyne.com>
2022-05-12 18:59:17 +0530

    secound function created

--- hello-world1/app.js
+++ hello-world1/app.js
@@ -20,7 +20,7 @@ exports.lambdaHandler = async (event, context) => {
         response = {
             'statusCode': 200,
             'body': JSON.stringify({
-                message: 'hello world......................................',
+                message: 'hello world1111111111111111111',
                 // location: ret.data.trim()
             })
         }
